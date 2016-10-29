789616b81ac1a562bceb41edadb871a95c4b46fc
Ilya Nee <neeilya@mail.ru>
2016-10-30 01:18:18 +0200

    Created and enabled separate component for header

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
-        <header>
-            <router-link to="/">Main</router-link>
-            <router-link to="statistics">Statistics</router-link>
-            <router-link to="about">About</router-link>
-        </header>
+        <app-header></app-header>
 
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import appHeader from './components/app-header.vue';
+
     export default {
         created() {
             console.log('Hello world!');
+        },
+        components: {
+            'app-header': appHeader
         }
     }
 </script>
---
+++ src/components/app-header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <router-link to="/">Main</router-link>
+        <router-link to="statistics">Statistics</router-link>
+        <router-link to="about">About</router-link>
+    </div>
+</template>
+
+<style scoped>
+    /* Scoped local styles */
+</style>
