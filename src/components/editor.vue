0d41eadb222b7145d06512a80655e113f711b26c
Ilya Nee <neeilya@mail.ru>
2016-11-01 20:13:37 +0100

    Basic form input + textarea in editor component

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,19 +1,73 @@
 <template>
     <md-card>
         <md-card-content>
-            this is editor component content
+            <input placeholder="Key">
+            <textarea placeholder="Value"></textarea>
+
+            <md-button class="md-icon-button md-raised md-violet action-button mx-0">
+                <md-icon>done</md-icon>
+                <md-tooltip md-direction="top">Create item/namespace</md-tooltip>
+            </md-button>
         </md-card-content>
     </md-card>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                //
+            }
+        },
         created() {
-            console.log('editor component has been rendered!');
+            //
         }
     }
 </script>
 
 <style scoped>
-    /* Local scoped styles */
+    input {
+        width: 100%;
+        border: 0;
+        font-weight: bold;
+        font-size: 120%;
+    }
+
+    textarea:focus, input:focus {
+        outline: none;
+    }
+
+    textarea {
+        font-size: 100%;
+        margin-top: 10px;
+        font-weight: normal;
+        border: none;
+        min-height: 295px;
+        resize: none;
+        width: 100%;
+    }
+
+    .md-card .md-card-content {
+        padding: 25px;
+    }
+
+    input::-webkit-input-placeholder {
+        font-weight: bold;
+    }
+
+    input:-moz-placeholder {
+        font-weight: bold;
+    }
+
+    input::-moz-placeholder {
+        font-weight: bold;
+    }
+
+    input:-ms-input-placeholder {  
+        font-weight: bold;
+    }
+
+    .md-violet, .md-violet:hover {
+        background-color: #6639b6 !important;
+    }
 </style>
