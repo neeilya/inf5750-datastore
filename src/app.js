d36b81190f27103e928d41b367dc7c0480360f5d
Ilya Nee <neeilya@mail.ru>
2016-11-01 14:26:24 +0100

    Enabled material vue package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.css';
 import App from './app.vue';
 import routes from './routes';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(VueMaterial);
+
+Vue.material.theme.register('default', {
+  primary: 'blue-grey',
+  accent: 'white'
+});
 
 const router = new VueRouter({
     routes: routes
