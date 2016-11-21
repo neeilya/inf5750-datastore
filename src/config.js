24b9cf2a6948fb8b72f4406509f5a56da1d4def6
Ilya Nee <neeilya@mail.ru>
2016-11-21 13:51:44 +0100

    Fetching metadata functionality in statistics

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
+    /**
+     * Base API URL
+     */
     api_base_url: 'https://play.dhis2.org/demo/api/25/dataStore'
 }
--- src/pages/statistics.vue
+++ src/pages/statistics.vue
@@ -1,20 +1,84 @@
 <template>
     <div>
-        <h1>Statistics page</h1>
-        <p>
-            this is statistics page
-        </p>
+        <div v-if="initializing" class="text-center">
+            <spinner class="spinner"></spinner>
+        </div>
+        <div v-else>
+            <md-card>
+                <md-toolbar>
+                    <span class="h3">Statistics</span>
+                </md-toolbar>
+                <md-card-content>
+                    <div v-for="namespace in namespaces">
+                        <pre>{{ namespace }}</pre>
+                    </div>
+                </md-card-content>
+            </md-card>
+        </div>
     </div>
 </template>
 
-<script>
+<script type="text/babel">
+    import spinner from 'components/assets/spinner.vue';
+    import config from 'config';
+
     export default {
+        data() {
+            return {
+                initializing: true,
+                namespaces: []
+            }
+        },
         created() {
-            //
+            this.fetchNamespaces().then( response => {
+                for(let i = 0; i < response.data.length; ++i) {
+                    let namespace = response.data[i];
+                    let lastNamespace = i === response.data.length - 1;
+
+                    this.namespaces.push({
+                        name: namespace,
+                        keys: []
+                    });
+
+                    this.fetchItemsForNamespace(namespace).then(response => {
+                        for(let j = 0; j < response.data.length; ++j) {
+                            let item = response.data[j];
+                            let lastItem = j === response.data.length - 1;
+
+                            this.fetchItem(namespace, item).then(response => {
+                                this.namespaces[i].keys.push({
+                                    lastUpdated: response.data.lastUpdated,
+                                    id: response.data.id
+                                });
+
+                                if(lastNamespace && lastItem) {
+                                    this.initializing = false;
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        },
+        components: {
+            spinner: spinner
+        },
+        methods: {
+            fetchNamespaces() {
+                return this.$http.get(`${ config.api_base_url }`);
+            },
+            fetchItemsForNamespace(namespace) {
+                return this.$http.get(`${ config.api_base_url }/${ namespace }`);
+            },
+            fetchItem(namespace, item) {
+                return this.$http.get(`${ config.api_base_url }/${ namespace }/${ item }/metaData`);
+            }
         }
     }
 </script>
 
 <style scoped>
-    /* Scoped local styles */
+    .spinner {
+        margin-top: 50px;
+    }
 </style>
