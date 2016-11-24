40ea9c3434141e04006863c4dd131d1770e82032
Ilya Nee <neeilya@mail.ru>
2016-11-24 15:50:44 +0100

    Populated api service with create/update requests for items/namespaces

--- src/services/api.js
+++ src/services/api.js
@@ -31,5 +31,50 @@ export default {
         return Vue.http.get(`${ config.api_base_url }/${ namespace }/${ item }/metaData`).catch(response => {
             throw response;
         });
+    },
+    /**
+     * Store new item
+     * @param namespace
+     * @param key
+     * @param body
+     * @returns {Promise.<T>}
+     */
+    createItem(namespace, key, body) {
+        return Vue.http.post(`${ config.api_base_url }/${ namespace }/${ key }`, body).catch(response => {
+            throw response;
+        });
+    },
+    /**
+     * Update existing item
+     * @param namespace
+     * @param key
+     * @param body
+     * @returns {Promise.<T>}
+     */
+    updateItem(namespace, key, body) {
+        return Vue.http.put(`${ config.api_base_url }/${ namespace }/${ key }`, body).catch(response => {
+            throw response;
+        });
+    },
+    /**
+     * Delete key from namespace
+     * @param namespace
+     * @param key
+     * @returns {Promise.<T>}
+     */
+    deleteItem(namespace, key) {
+        return Vue.http.delete(`${ config.api_base_url }/${ namespace }/${ key }`).catch(response => {
+            throw response;
+        });
+    },
+    /**
+     * Delete namespace
+     * @param namespace
+     * @returns {Promise.<T>}
+     */
+    deleteNamespace(namespace) {
+        return Vue.http.delete(`${ config.api_base_url }/${ namespace }`).catch(response => {
+            throw response;
+        });
     }
 };
